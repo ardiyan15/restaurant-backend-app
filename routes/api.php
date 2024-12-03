b75be40317afc7efa785831c65902d497b389f98
ardiyan15 <ardhiyan15@gmail.com>
2024-12-04 06:50:31 +0700

    remove unused route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\RestaurantController;
 use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
