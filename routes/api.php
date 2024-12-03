ddf9b7ade6021a0ffcd6033ba1fc74c69c25a845
ardiyan15 <ardhiyan15@gmail.com>
2024-12-04 06:32:38 +0700

    update middleware

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
 });
 
-Route::middleware(['auth:sanctum'. CheckRole::class])->get('/restaurants', [RestaurantController::class, 'index']);
-Route::middleware('auth:sanctum')->post('/restaurant', [RestaurantController::class, 'store']);
+Route::middleware('auth:sanctum')->get('/restaurants', [RestaurantController::class, 'index']);
+Route::middleware(['auth:sanctum', CheckRole::class])->post('/restaurant', [RestaurantController::class, 'store']);
